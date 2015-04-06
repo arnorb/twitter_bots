b44b8c49898e142373d53aa926abfbd713f22772
Arnór Bogason <arnor@arnor.is>
2015-04-06 13:33:42 +0000

    cleanup file

--- borgarstjorn.py
+++ borgarstjorn.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import calendar
 import tweepy
 
+
 today = datetime.today().date()
 current_month = today.month
 current_year = today.year
@@ -16,16 +17,16 @@ first_tuesday = calendar.Calendar(1).monthdatescalendar(current_year,current_mon
 third_tuesday = calendar.Calendar(1).monthdatescalendar(current_year,current_month)[3][0]
 fundir = [first_tuesday, third_tuesday, today]
 
-CONSUMER_KEY = 'qs05jklgCleQ3c7hIA2OvbOV4'
-CONSUMER_SECRET = ''
+CONSUMER_KEY = 'BpHltB8IF8hZ5vZSveYhOPOgg'
+CONSUMER_SECRET = '5Kv1YBKxtQIoOoPN4DEwXVQXXMi8iXFIUjJzCeuomFJyg9Ats1'
 ACCESS_TOKEN = '3137303357-0YCjcqiVZuKm6w1OBxNo7wKohEJ4KO9j5gUjHTe'
-ACCESS_SECRET = ''
+ACCESS_SECRET = '6rFOxwRGAsOr3mCVBz2KrdgQns4caIpmezIpqc7lVq7RY'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-message = 'Borgarstjórnarfundur hefst kl. 14:00'
+message = 'Borgarstjórnarfundur hefst í dag kl. 14:00'
 
 if today in fundir:
     api.update_status(status=message)
